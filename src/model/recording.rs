2d66df62a528e4231123b952af8550d3a146ec9c
okno <paul.delafosse@protonmail.com>
2019-07-13 15:56:13 +0200

    feat(model): add recording

---
+++ src/model/recording.rs
@@ -0,0 +1,30 @@
+use crate::model::artist::Artist;
+
+/// A recording is an entity in MusicBrainz which can be linked to tracks on releases. Each track
+/// must always be associated with a single recording, but a recording can be linked to any number
+/// of tracks.
+/// A recording represents distinct audio that has been used to produce at least one released track
+/// through copying or mastering. A recording itself is never produced solely through copying or
+/// mastering.
+/// Generally, the audio represented by a recording corresponds to the audio at a stage in the
+/// production process before any final mastering but after any editing or mixing.
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Recording {
+
+    /// See MusicBrainz Identifier. TODO: indoc link to mb doc
+    pub id: String,
+
+    /// The title of the recording.
+    pub title: String,
+
+    pub video: bool,
+
+    /// The length of the recording. It's only entered manually for standalone recordings.
+    /// For recordings that are being used on releases, the recording length is the median length of
+    /// all tracks (that have a track length) associated with that recording. If there is an even
+    /// number of track lengths, the smaller median candidate is used.
+    pub length: u32, // TODO: CUSTOM Deserialized to make this a duration
+
+    /// See Disambiguation Comment.
+    pub disambiguation: String,
+}
