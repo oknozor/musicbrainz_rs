21cfdeac37d625bbb11a64ca58b7cc0d42e9df4c
okno <paul.delafosse@protonmail.com>
2019-07-13 17:26:04 +0200

    doc(): update manifest

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,8 +1,8 @@
 
-extern crate music_brainz;
+extern crate music_brainz_rs;
 
-use music_brainz::model::artist::Artist;
-use music_brainz::QueryAble;
+use music_brainz_rs::model::artist::Artist;
+use music_brainz_rs::QueryAble;
 
 fn main() {
     let nirvana  = Artist::by_id("5b11f4ce-a62d-471e-81fc-a69a8278c7da");
