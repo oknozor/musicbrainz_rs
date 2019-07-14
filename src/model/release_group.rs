78731b5495dc944a40d61bd016047715224674e2
okno <paul.delafosse@protonmail.com>
2019-07-14 13:13:28 +0200

    refactor(model): use serde global deserialization policy to force kebab-case globally

--- src/model/release_group.rs
+++ src/model/release_group.rs
@@ -2,28 +2,24 @@ use chrono::NaiveDate;
 use crate::{date_format};
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all(deserialize = "kebab-case"))]
 pub struct ReleaseGroup {
 
     /// See [MusicBrainz Identifier](https://musicbrainz.org/doc/MusicBrainz_Identifier).
     pub id: String,
 
-    #[serde(rename = "primary-type-id")]
     pub primary_type_id: String,
 
     /// The type describes what kind of releases the release group represents, for example album,
     /// single, soundtrack, compilation etc.
     /// See the Type subpage for a full list of release group types.
-    #[serde(rename = "primary-type")]
     pub primary_type: String,
 
-    #[serde(rename = "secondary-type-ids")]
     pub secondary_type_ids: Vec<String>,
 
-    #[serde(rename = "secondary-types")]
     pub secondary_types: Vec<String>,
 
     #[serde(deserialize_with = "date_format::deserialize")]
-    #[serde(rename = "first-release-date")]
     pub first_release_date: NaiveDate,
 
     /// The title of a release group is usually very similar, if not the same, as the titles of the
