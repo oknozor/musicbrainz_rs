ce2021f24ed9f073dc29349f2a6449cf49c55799
okno <paul.delafosse@protonmail.com>
2019-07-15 13:10:02 +0200

    feat(model): add work

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -3,11 +3,13 @@ use crate::model::artist::Artist;
 use crate::model::recording::Recording;
 use crate::model::release_group::ReleaseGroup;
 use crate::model::release::Release;
+use crate::model::work::Work;
 
 pub mod artist;
 pub mod recording;
 pub mod release_group;
 pub mod release;
+pub mod work;
 
 impl QueryAble<'_> for Artist {
     fn path() -> &'static str {
@@ -32,3 +34,10 @@ impl QueryAble<'_> for Release {
         "release"
     }
 }
+
+impl QueryAble<'_> for Work {
+    fn path() -> &'static str {
+        "work"
+    }
+}
+
