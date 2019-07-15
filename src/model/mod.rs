239df35518908c590019b3b8721ad96cf04894dc
okno <paul.delafosse@protonmail.com>
2019-07-15 13:13:55 +0200

    feat(model): add label

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -4,12 +4,14 @@ use crate::model::recording::Recording;
 use crate::model::release_group::ReleaseGroup;
 use crate::model::release::Release;
 use crate::model::work::Work;
+use crate::model::label::Label;
 
 pub mod artist;
 pub mod recording;
 pub mod release_group;
 pub mod release;
 pub mod work;
+pub mod label;
 
 impl QueryAble<'_> for Artist {
     fn path() -> &'static str {
@@ -38,6 +40,12 @@ impl QueryAble<'_> for Release {
 impl QueryAble<'_> for Work {
     fn path() -> &'static str {
         "work"
+    }
+}
+
+impl QueryAble<'_> for Label {
+    fn path() -> &'static str {
+        "label"
     }
 }
 
