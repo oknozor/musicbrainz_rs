ebf4edc428cf5d8cf18576760d2bccc0f7ba7717
okno <paul.delafosse@protonmail.com>
2019-07-15 13:10:02 +0200

    feat(model): add work

---
+++ src/model/work.rs
@@ -0,0 +1,15 @@
+/// In MusicBrainz terminology, a work is a distinct intellectual or artistic creation, which can be
+/// expressed in the form of one or more audio recordings. While a work in MusicBrainz is usually
+/// musical in nature, it is not necessarily so. For example, a work could be a novel, play,
+/// poem or essay, later recorded as an oratory or audiobook.
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all(deserialize = "kebab-case"))]
+pub struct Work {
+    /// See [MusicBrainz Identifier](https://musicbrainz.org/doc/MusicBrainz_Identifier).
+    pub id: String,
+    pub type_id: String,
+    #[serde(rename = "type")]
+    pub work_type: String,
+    pub language: String,
+    pub disambiguation: String,
+}
