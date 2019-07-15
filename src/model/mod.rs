55d33aeb1ef839fefef6c686bd16c0bb289bb3c7
okno <paul.delafosse@protonmail.com>
2019-07-15 13:46:31 +0200

    feat(model): add area: GET, move area and lifespan to dedicated modules

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,10 +1,11 @@
-use crate::QueryAble;
 use crate::model::artist::Artist;
-use crate::model::recording::Recording;
-use crate::model::release_group::ReleaseGroup;
-use crate::model::release::Release;
-use crate::model::work::Work;
 use crate::model::label::Label;
+use crate::model::recording::Recording;
+use crate::model::release::Release;
+use crate::model::release_group::ReleaseGroup;
+use crate::model::work::Work;
+use crate::QueryAble;
+use crate::model::area::Area;
 
 pub mod artist;
 pub mod recording;
@@ -12,6 +13,8 @@ pub mod release_group;
 pub mod release;
 pub mod work;
 pub mod label;
+pub mod area;
+pub mod lifespan;
 
 impl QueryAble<'_> for Artist {
     fn path() -> &'static str {
@@ -48,4 +51,11 @@ impl QueryAble<'_> for Label {
         "label"
     }
 }
+
+impl QueryAble<'_> for Area {
+    fn path() -> &'static str {
+        "area"
+    }
+}
+
 
