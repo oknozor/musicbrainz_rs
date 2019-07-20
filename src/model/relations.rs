35c682dd47551e3b4a5508f394942bd239146fa7
okno <paul.delafosse@protonmail.com>
2019-07-20 15:22:42 +0200

    style(): rustfmt all

--- src/model/relations.rs
+++ src/model/relations.rs
@@ -5,36 +5,36 @@ use std::collections::HashMap;
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all(deserialize = "kebab-case"))]
 pub struct Relation {
-   #[serde(deserialize_with = "date_format::deserialize_opt")]
-   pub end: Option<NaiveDate>,
-   pub attributes: Vec<String>,
+    #[serde(deserialize_with = "date_format::deserialize_opt")]
+    pub end: Option<NaiveDate>,
+    pub attributes: Vec<String>,
     #[serde(flatten)]
-   pub content: RelationContent,
-   pub attribute_values: HashMap<String, String>,
-   pub attribute_ids: HashMap<String, String>,
-   pub target_credit: String,
-   pub source_credit: String,
-   pub ended: bool,
-   pub type_id: String,
-   #[serde(deserialize_with = "date_format::deserialize_opt")]
-   pub begin: Option<NaiveDate>,
-   pub direction: String,
-   #[serde(rename = "type")]
-   pub relation_type: String,
+    pub content: RelationContent,
+    pub attribute_values: HashMap<String, String>,
+    pub attribute_ids: HashMap<String, String>,
+    pub target_credit: String,
+    pub source_credit: String,
+    pub ended: bool,
+    pub type_id: String,
+    #[serde(deserialize_with = "date_format::deserialize_opt")]
+    pub begin: Option<NaiveDate>,
+    pub direction: String,
+    #[serde(rename = "type")]
+    pub relation_type: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all(deserialize = "kebab-case"))]
 pub enum RelationContent {
-   Artist(ArtistRelation),
+    Artist(ArtistRelation),
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all(deserialize = "kebab-case"))]
 pub struct ArtistRelation {
-   pub sort_name: String,
-   pub id: String,
-   pub aliases: Option<Vec<String>>,
-   pub disambiguation: String,
-   pub name: String,
+    pub sort_name: String,
+    pub id: String,
+    pub aliases: Option<Vec<String>>,
+    pub disambiguation: String,
+    pub name: String,
 }
