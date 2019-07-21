ca430aeb36c842498c43a4df73c1509912ae2ff0
okno <paul.delafosse@protonmail.com>
2019-07-21 15:24:40 +0200

    refactor(include): make artists include tests more explicit

--- tests/artist_includes_tests.rs
+++ tests/artist_includes_tests.rs
@@ -3,21 +3,23 @@ extern crate musicbrainz_rs;
 
 use musicbrainz_rs::model::artist;
 use musicbrainz_rs::model::artist::*;
+use musicbrainz_rs::model::relations::*;
 use musicbrainz_rs::QueryAble;
 use std::{thread, time};
 
 #[test]
 fn should_get_artist_releases() {
     let john_lee_hooker = Artist::fetch()
-        .id("5b11f4ce-a62d-471e-81fc-a69a8278c7da")
+        .id("b0122194-c49a-46a1-ade7-84d1d76bd8e9")
         .include(artist::Include::Releases)
         .execute()
         .unwrap();
 
-    let releases = john_lee_hooker.releases;
+    let releases = john_lee_hooker.releases.unwrap();
 
-    assert!(releases.is_some());
-    assert!(!releases.unwrap().is_empty());
+    assert!(releases
+        .iter()
+        .any(|release| release.title == "Boogie Chillen’ / Sally Mae"));
 
     thread::sleep(time::Duration::from_secs(1));
 }
@@ -25,15 +27,16 @@ fn should_get_artist_releases() {
 #[test]
 fn should_get_artist_works() {
     let john_lee_hooker = Artist::fetch()
-        .id("5b11f4ce-a62d-471e-81fc-a69a8278c7da")
+        .id("b0122194-c49a-46a1-ade7-84d1d76bd8e9")
         .include(artist::Include::Works)
         .execute()
         .unwrap();
 
-    let works = john_lee_hooker.works;
+    let works = john_lee_hooker.works.unwrap();
 
-    assert!(works.is_some());
-    assert!(!works.unwrap().is_empty());
+    assert!(works
+        .iter()
+        .any(|work| work.title == "Baby Please Don't Go"));
 
     thread::sleep(time::Duration::from_secs(1));
 }
@@ -41,15 +44,17 @@ fn should_get_artist_works() {
 #[test]
 fn should_get_artist_release_groups() {
     let john_lee_hooker = Artist::fetch()
-        .id("5b11f4ce-a62d-471e-81fc-a69a8278c7da")
+        .id("b0122194-c49a-46a1-ade7-84d1d76bd8e9")
         .include(artist::Include::ReleaseGroups)
         .execute()
         .unwrap();
 
-    let release_groups = john_lee_hooker.release_groups;
+    let release_groups = john_lee_hooker.release_groups.unwrap();
 
-    assert!(release_groups.is_some());
-    assert!(!release_groups.unwrap().is_empty());
+    assert!(release_groups.iter().any(|group| group.title == "Burnin’"));
+    assert!(release_groups
+        .iter()
+        .any(|group| group.title == "Travelin’"));
 
     thread::sleep(time::Duration::from_secs(1));
 }
@@ -57,15 +62,16 @@ fn should_get_artist_release_groups() {
 #[test]
 fn should_get_artist_recordings() {
     let john_lee_hooker = Artist::fetch()
-        .id("5b11f4ce-a62d-471e-81fc-a69a8278c7da")
+        .id("b0122194-c49a-46a1-ade7-84d1d76bd8e9")
         .include(artist::Include::Recordings)
         .execute()
         .unwrap();
 
-    let recordings = john_lee_hooker.recordings;
+    let recordings = john_lee_hooker.recordings.unwrap();
 
-    assert!(recordings.is_some());
-    assert!(!recordings.unwrap().is_empty());
+    assert!(recordings
+        .iter()
+        .any(|recording| recording.title == "A Little Bit Higher"));
 
     thread::sleep(time::Duration::from_secs(1));
 }
@@ -73,29 +79,38 @@ fn should_get_artist_recordings() {
 #[test]
 fn should_get_artist_aliases() {
     let john_lee_hooker = Artist::fetch()
-        .id("5b11f4ce-a62d-471e-81fc-a69a8278c7da")
+        .id("b0122194-c49a-46a1-ade7-84d1d76bd8e9")
         .include(artist::Include::Aliases)
         .execute()
         .unwrap();
 
     let aliases = john_lee_hooker.aliases;
 
-    assert!(aliases.is_some());
-    assert!(!aliases.unwrap().is_empty());
+    assert!(aliases
+        .unwrap()
+        .iter()
+        .any(|alias| alias.name == "Delta John"));
 }
 
 #[test]
 fn should_get_artist_artist_relations() {
     let john_lee_hooker = Artist::fetch()
-        .id("5b11f4ce-a62d-471e-81fc-a69a8278c7da")
+        .id("b0122194-c49a-46a1-ade7-84d1d76bd8e9")
         .include(artist::Include::ArtistRelations)
         .execute()
         .unwrap();
 
-    let relations = john_lee_hooker.relations;
+    let relations = john_lee_hooker.relations.unwrap();
 
-    assert!(relations.is_some());
-    assert!(!relations.unwrap().is_empty());
+    assert!(relations.iter().any(|rel| rel.relation_type == "parent"));
+    assert!(relations.iter().any(|rel| rel.content
+        == RelationContent::Artist(ArtistRelation {
+            sort_name: "Hooker, Zakiya".to_string(),
+            id: "8f7fbe13-da00-44ab-bb5b-4da8c18367f0".to_string(),
+            disambiguation: "".to_string(),
+            name: "Zakiya Hooker".to_string(),
+            aliases: None,
+        })));
 
     thread::sleep(time::Duration::from_secs(1));
 }
