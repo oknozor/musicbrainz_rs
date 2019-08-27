ddbbcefc8026c67bd7ea5c7cf07152dfdfc86497
okno <paul.delafosse@protonmail.com>
2019-08-27 13:09:52 +0200

    feat(config): make http client configurable

---
+++ examples/set_user_agent.rs
@@ -0,0 +1,14 @@
+extern crate musicbrainz_rs;
+
+use musicbrainz_rs::config;
+use musicbrainz_rs::model::artist::*;
+use musicbrainz_rs::QueryAble;
+
+fn main() {
+    config::set_user_agent("joe");
+    let nirvana = Artist::fetch()
+        .id("5b11f4ce-a62d-471e-81fc-a69a8278c7da")
+        .execute();
+
+    assert_eq!(nirvana.unwrap().name, "Nirvana".to_string());
+}
---
+++ src/config.rs
@@ -0,0 +1,56 @@
+use reqwest::header;
+use reqwest::Client;
+use reqwest::RequestBuilder;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+pub struct MusicBrainzClient(Arc<Mutex<Client>>);
+
+lazy_static! {
+    pub static ref HTTP_CLIENT: MusicBrainzClient = { init_http_client() };
+}
+
+impl MusicBrainzClient {
+    pub fn get(&self, path: &str) -> RequestBuilder {
+        let client_ref = Arc::clone(&HTTP_CLIENT.0);
+        let client_lock = client_ref.lock().expect("Unable to get musicbrainz client");
+        client_lock.get(path)
+    }
+}
+
+fn init_http_client() -> MusicBrainzClient {
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+        header::USER_AGENT,
+        header::HeaderValue::from_static("musicbrainz_rs default"),
+    );
+    println!("{:?}", headers);
+    let client = reqwest::Client::builder()
+                .default_headers(headers)
+                .build().expect("Unable to set default user agent, the following values must be set in Cargo.toml : 'name', 'version', 'authors'");
+
+    MusicBrainzClient {
+        0: Arc::new(Mutex::new(client)),
+    }
+}
+
+pub fn set_user_agent(user_agent: &'static str) {
+    let client_ref = Arc::clone(&HTTP_CLIENT.0);
+    let mut client_lock = client_ref.lock().expect("Unable to set musicbrainz client");
+
+    let mut headers = header::HeaderMap::new();
+
+    headers.insert(
+        header::USER_AGENT,
+        header::HeaderValue::from_static(user_agent),
+    );
+
+    *client_lock = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()
+        .expect("Unable to set user agent");
+}
+
+pub const BASE_URL: &str = "http://musicbrainz.org/ws/2";
+pub const FMT_JSON: &str = "?fmt=json";
+pub const PARAM_INC: &str = "&inc=";
