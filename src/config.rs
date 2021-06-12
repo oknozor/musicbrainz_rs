a774023be0aed2469a19c78cbeb823529b84ad52
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2021-06-13 00:48:10 +0530

    feat(): implement auto-retries

--- src/config.rs
+++ src/config.rs
@@ -1,18 +1,24 @@
 use reqwest::header;
 use reqwest::Client;
+use reqwest::Error;
 use reqwest::RequestBuilder;
+use reqwest::Response;
 use std::sync::Arc;
 use std::sync::Mutex;
+use std::{thread, time::Duration};
 
 pub(crate) const BASE_URL: &str = "http://musicbrainz.org/ws/2";
 pub(crate) const BASE_COVERART_URL: &str = "http://coverartarchive.org";
 pub(crate) const FMT_JSON: &str = "?fmt=json";
 pub(crate) const PARAM_INC: &str = "&inc=";
+const HTTP_RATELIMIT_CODE: u16 = 503;
 
 pub(crate) struct MusicBrainzClient(Arc<Mutex<Client>>);
+struct MusicBrainzRetries(Arc<Mutex<u32>>);
 
 lazy_static! {
     pub(crate) static ref HTTP_CLIENT: MusicBrainzClient = init_http_client();
+    static ref HTTP_RETRIES: MusicBrainzRetries = init_http_retries();
 }
 
 impl MusicBrainzClient {
@@ -20,6 +26,26 @@ impl MusicBrainzClient {
         let client_ref = Arc::clone(&HTTP_CLIENT.0);
         let client_lock = client_ref.lock().expect("Unable to get musicbrainz client");
         client_lock.get(path)
+    }
+
+    pub(crate) fn send_with_retries(&self, request: RequestBuilder) -> Result<Response, Error> {
+        let mut retries = *HTTP_RETRIES.0.lock().unwrap();
+        loop {
+            let request = request.try_clone().unwrap();
+            let response = request.send()?;
+            if response.status().as_u16() == HTTP_RATELIMIT_CODE && retries > 0 {
+                let headers = response.headers();
+                let retry_secs = headers.get("retry-after").unwrap().to_str().unwrap();
+                // It seems like the value in the response header is sometimes rounded-off to the
+                // lower number, which can be lower than when the server actually accepts the next
+                // request. So we add one to the received duration to account for this.
+                let duration = Duration::from_secs(retry_secs.parse::<u64>().unwrap() + 1);
+                thread::sleep(duration);
+                retries -= 1;
+            } else {
+                break Ok(response);
+            }
+        }
     }
 }
 
@@ -36,6 +62,13 @@ fn init_http_client() -> MusicBrainzClient {
 
     MusicBrainzClient {
         0: Arc::new(Mutex::new(client)),
+    }
+}
+
+fn init_http_retries() -> MusicBrainzRetries {
+    let retries = 2;
+    MusicBrainzRetries {
+        0: Arc::new(Mutex::new(retries)),
     }
 }
 
@@ -65,4 +98,12 @@ pub fn set_user_agent(user_agent: &'static str) {
         .default_headers(headers)
         .build()
         .expect("Unable to set user agent");
+}
+
+pub fn set_default_retries(retries: u32) {
+    let retries_ref = Arc::clone(&HTTP_RETRIES.0);
+    let mut retries_lock = retries_ref
+        .lock()
+        .expect("Unable to set musicbrainz client retries");
+    *retries_lock = retries;
 }
