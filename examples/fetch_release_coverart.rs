85b38e48b238ddb5a9c910edf9234aae17650ced
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2021-06-24 12:06:20 +0530

    feat(coverart): implement get_coverart method

--- examples/fetch_release_coverart.rs
+++ examples/fetch_release_coverart.rs
@@ -1,6 +1,7 @@
 extern crate musicbrainz_rs;
 
 use musicbrainz_rs::entity::release::*;
+use musicbrainz_rs::prelude::*;
 use musicbrainz_rs::FetchCoverart;
 
 fn main() {
@@ -8,6 +9,19 @@ fn main() {
         .id("76df3287-6cda-33eb-8e9a-044b5e15ffdd")
         .execute()
         .expect("Unable to get cover art");
+
+    assert_eq!(in_utero_coverart.images[0].front, true);
+    assert_eq!(in_utero_coverart.images[0].back, false);
+
+    let in_utero = Release::fetch()
+        .id("76df3287-6cda-33eb-8e9a-044b5e15ffdd")
+        .execute()
+        .expect("Unable to get release");
+
+    let in_utero_coverart = in_utero
+        .get_coverart()
+        .execute()
+        .expect("Unable to get coverart");
 
     assert_eq!(in_utero_coverart.images[0].front, true);
     assert_eq!(in_utero_coverart.images[0].back, false);
--- tests/release_group/mod.rs
+++ tests/release_group/mod.rs
@@ -1,2 +1,3 @@
 mod release_group_browse;
+mod release_group_coverart;
 mod release_group_includes;
