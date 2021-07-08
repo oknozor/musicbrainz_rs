b23759a92df0aef58b9ce0542033caed456a0a55
Paul Delafosse <paul.delafosse@protonmail.com>
2021-07-08 08:51:54 +0200

    feat: implement search for release-groups

---
+++ tests/release_group/release_group_search.rs
@@ -0,0 +1,13 @@
+extern crate musicbrainz_rs;
+
+use musicbrainz_rs::entity::release_group::*;
+use musicbrainz_rs::Search;
+
+#[test]
+fn should_search_artist() {
+    let query = ReleaseGroup::query_builder().release("Tonight").build();
+
+    let result = ReleaseGroup::search(query).execute().unwrap();
+
+    assert!(!result.entities.is_empty());
+}
