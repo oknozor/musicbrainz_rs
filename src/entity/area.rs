c80ab3137248949aa337661fca06821e89d25268
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2021-07-08 22:38:10 +0530

    feat(area): support additional relation incs
    - Area
    - Event
    - Recording
    - Release
    - Url
    - Work
    
    Also removed Artist relation include since it doesn't seem to be
    supported, after trying it out on a few different area entities.

--- src/entity/area.rs
+++ src/entity/area.rs
@@ -2,6 +2,7 @@ use super::{Include, Relationship, Subquery};
 use crate::entity::alias::Alias;
 use crate::entity::genre::Genre;
 use crate::entity::lifespan::LifeSpan;
+use crate::entity::relations::Relation;
 use crate::entity::tag::Tag;
 use crate::entity::BrowseBy;
 
@@ -16,6 +17,7 @@ pub struct Area {
     pub disambiguation: String,
     pub name: String,
     pub sort_name: String,
+    pub relations: Option<Vec<Relation>>,
     pub iso_3166_1_codes: Option<Vec<String>>,
     pub life_span: Option<LifeSpan>,
     pub tags: Option<Vec<Tag>>,
@@ -28,10 +30,12 @@ impl_browse!(Area, (by_collection, BrowseBy::Collection));
 
 impl_includes!(
     Area,
-    (
-        with_artist_relations,
-        Include::Relationship(Relationship::Artist)
-    ),
+    (with_area_relations, Include::Relationship(Relationship::Area)),
+    (with_event_relations, Include::Relationship(Relationship::Event)),
+    (with_recording_relations, Include::Relationship(Relationship::Recording)),
+    (with_release_relations, Include::Relationship(Relationship::Release)),
+    (with_url_relations, Include::Relationship(Relationship::Url)),
+    (with_work_relations, Include::Relationship(Relationship::Work)),
     (with_tags, Include::Subquery(Subquery::Tags)),
     (with_aliases, Include::Subquery(Subquery::Aliases)),
     (with_genres, Include::Subquery(Subquery::Genres)),
